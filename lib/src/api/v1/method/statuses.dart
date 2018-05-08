c2f403699ce7193f49075f14dab68d8f9eab00cc
sftblw <ccwpc@hanmail.net>
2018-05-09 01:09:13 +0900

    API statuses todo as comment

--- lib/src/api/v1/method/statuses.dart
+++ lib/src/api/v1/method/statuses.dart
@@ -8,7 +8,19 @@ import '../../../entities/v1/entities.dart';
 
 const _prefix = '/api/v1/statuses';
 
+// const _StatusesGet statusesGet = const _StatusesGet();
+// const _StatusesContext statusesContext = const _StatusesContext();
+// const _StatusesCard statusesCard = const _StatusesCard();
+// const _StatusesRebloggedBy statusesRebloggedBy = const _StatusesRebloggedBy();
+// const _StatusesFavoratedBy statusesFavoratedBy = const _StatusesFavoratedBy();
 const _StatusesPost statusesPost = const _StatusesPost();
+// const _StatusesDelete statusesDelete = const _StatusesDelete();
+// const _StatusesReblog statusesReblog = const _StatusesReblog();
+// const _StatusesUnreblog statusesUnreblog = const _StatusesUnreblog();
+// const _StatusesFavorate statusesFavorate = const _StatusesFavorate();
+// const _StatusesUnfavorate statusesUnfavorate = const _StatusesUnfavorate();
+// const _StatusesMute statusesMute = const _StatusesMute();
+// const _StatusesUnmute statusesUnmute = const _StatusesUnmute();
 
 class _StatusesPost extends Endpoint {
   const _StatusesPost() : super(method: HttpMethod.POST, url: _prefix);
